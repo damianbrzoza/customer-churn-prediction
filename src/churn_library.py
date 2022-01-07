f0e82499c8666e10c3c355d58506cd727f0ccb4a
DamianBrzoza <damianbrzoza95@gmail.com>
2022-01-08 00:11:37 +0100

    organize and finish main code

--- src/churn_library.py
+++ src/churn_library.py
@@ -1,25 +1,40 @@
 """Module enable customer churn analysis."""
+from copy import deepcopy
+from typing import Any, Dict, List, Tuple
+
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from sklearn.base import ClassifierMixin
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import RocCurveDisplay, classification_report
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.preprocessing import StandardScaler
 
 from config import CAT_COLUMNS, QUANT_COLUMNS
 from src.plot_chart import (
+    compare_roc_curve,
     plot_boxplot_with_category,
     plot_categorical_proportion,
+    plot_classifier_summary_as_image,
     plot_correlations,
     plot_quant_histogram,
     plot_target_proportion,
+    show_features_importance,
 )
 
 
-def import_data(pth: str, verbose=False) -> pd.DataFrame:
+def import_data(pth: str, verbose: bool = False) -> pd.DataFrame:
     """
-    Returns dataframe for the csv found at pth.
+    Return dataframe for the csv found at pth.
+
     :param pth: a path to the csv
     :type pth: str
     :return: pandas dataframe with imported data
     :rtype: pd.DataFrame
     """
-
     df = pd.read_csv(pth, index_col=0)
 
     df[CAT_COLUMNS] = df[CAT_COLUMNS].astype("category")
@@ -38,94 +53,158 @@ def perform_eda(df: pd.DataFrame) -> None:
     :param df: pandas dataframe
     :type df: pd.DataFrame
     """
-    print(df.head())
-    print(df.shape)
-    print(df.isnull().sum())
-    print(df.describe())
+    data = deepcopy(df)
 
-    df["Churn"] = (
-        df["Attrition_Flag"]
+    print(data.head())
+    print(data.shape)
+    print(data.isnull().sum())
+    print(data.describe())
+
+    data["Churn"] = (
+        data["Attrition_Flag"]
         .apply(lambda val: 0 if val == "Existing Customer" else 1)
         .astype("int64")
     )
 
     # Get some info about correlation between our target and numerical values
 
-    plot_correlations(df)
-    plot_target_proportion(df)  # Univariate, quantitative plot
+    plot_correlations(data)
+    plot_target_proportion(data)  # Univariate, quantitative plot
 
-    plot_quant_histogram(df, column_name=QUANT_COLUMNS[0])  # Univariate, categorical plot
+    plot_quant_histogram(data, column_name=QUANT_COLUMNS[0])  # Univariate, categorical plot
 
     for cat in CAT_COLUMNS[1:]:  # without Attrition_Flag
-        plot_categorical_proportion(df, column_name=cat)  # Bivariate plot
+        plot_categorical_proportion(data, column_name=cat)  # Bivariate plot
 
-    plot_boxplot_with_category(df, column_name=QUANT_COLUMNS[0])  # Bivariate plot
+    plot_boxplot_with_category(data, column_name=QUANT_COLUMNS[0])  # Bivariate plot
 
 
-def perform_feature_engineering(df, response):
+def perform_feature_engineering(
+    df: pd.DataFrame,
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     """
-    input:
-        df: pandas dataframe
-        response: string of response name [optional argument that could be used for naming variables or index y column]
+    Enable efficient training on that data. Return splitted dataset.
 
-    output:
-        X_train: X training data
-        X_test: X testing data
-        y_train: y training data
-        y_test: y testing data
+    :param df: data
+    :type df: pd.DataFrame
+    :return: X_train, X_test, y_train, y_test
+    :rtype: Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]
     """
+    df.drop(columns="CLIENTNUM", inplace=True)  # not store any valuable information
 
-    return train_test_split(X, y, test_size=0.3, random_state=42)
+    df[QUANT_COLUMNS] = StandardScaler().fit_transform(
+        df[QUANT_COLUMNS]
+    )  # apply for logistic regression
+
+    df[["Education_Level", "Marital_Status", "Income_Category"]] = df[
+        ["Education_Level", "Marital_Status", "Income_Category"]
+    ].applymap(
+        lambda x: np.nan if x == "Unknown" else x
+    )  # correct form for one-hot encoding
+
+    df = pd.get_dummies(df[CAT_COLUMNS[1:]]).join(df).drop(columns=df[CAT_COLUMNS[1:]])
+
+    return train_test_split(
+        df.drop(columns="Attrition_Flag"),
+        df["Attrition_Flag"],
+        test_size=0.3,
+        random_state=42,
+        stratify=df["Attrition_Flag"],
+    )
 
 
-def classification_report_image(
-    y_train, y_test, y_train_preds_lr, y_train_preds_rf, y_test_preds_lr, y_test_preds_rf
-):
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.Series,
+    y_test: pd.Series,
+    classifier: ClassifierMixin,
+    classifier_name: str,
+    param_grid: Dict[str, List[Any]],
+) -> ClassifierMixin:
     """
-    produces classification report for training and testing results and stores report as image
-    in images folder
-    input:
-        y_train: training response values
-        y_test:  test response values
-        y_train_preds_lr: training predictions from logistic regression
-        y_train_preds_rf: training predictions from random forest
-        y_test_preds_lr: test predictions from logistic regression
-        y_test_preds_rf: test predictions from random forest
+    Train, store model results: images + scores, and store models.
 
-    output:
-        None
+    :param X_train: train features
+    :type X_train: pd.DataFrame
+    :param X_test: test features
+    :type X_test: pd.DataFrame
+    :param y_train: train labels
+    :type y_train: pd.Series
+    :param y_test: test labels
+    :type y_test: pd.Series
+    :param classifier: classifier/model
+    :type classifier: ClassifierMixin
+    :param classifier_name: name of classifier
+    :type classifier_name: str
+    :param param_grid: parameteres to be checked for given classifier
+    :type param_grid: Dict[str, List[Any]]
+    :return: trained model
+    :rtype: ClassifierMixin
     """
-    pass
+    classifier = GridSearchCV(
+        estimator=classifier, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2
+    )
+    classifier.fit(X_train, y_train)
 
+    y_train_preds = classifier.best_estimator_.predict(X_train)
+    y_test_preds = classifier.best_estimator_.predict(X_test)
 
-def feature_importance_plot(model, X_data, output_pth):
-    """
-    creates and stores the feature importances in pth
-    input:
-        model: model object containing feature_importances_
-        X_data: pandas dataframe of X values
-        output_pth: path to store the figure
+    # scores
+    print(f"{classifier_name} results")
+    print("test results")
+    print(classification_report(y_test, y_test_preds))
+    print("train results")
+    print(classification_report(y_train, y_train_preds))
 
-    output:
-        None
-    """
-    pass
+    # plot
+    RocCurveDisplay.from_estimator(estimator=classifier.best_estimator_, X=X_test, y=y_test)
+    plt.show()
 
+    # save best model
+    joblib.dump(classifier.best_estimator_, f"./models/{classifier_name}.pkl")
 
-def train_models(X_train, X_test, y_train, y_test):
-    """
-    train, store model results: images + scores, and store models
-    input:
-        X_train: X training data
-        X_test: X testing data
-        y_train: y training data
-        y_test: y testing data
-    output:
-        None
-    """
-    pass
+    return classifier
 
 
 if __name__ == "__main__":
     df = import_data("./data/bank_data.csv", verbose=False)
-    # perform_eda(df)
+    perform_eda(df)
+    X_train, X_test, y_train, y_test = perform_feature_engineering(df)
+
+    param_grid_rfc_model: Dict[str, List[Any]] = {
+        "n_estimators": [200, 500],
+        "max_features": ["auto", "sqrt"],
+        "max_depth": [4, 5, 100],
+        "criterion": ["gini", "entropy"],
+    }
+
+    param_grid_logistic_model = {"C": [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+
+    rfc_model = train_model(
+        X_train=X_train,
+        X_test=X_test,
+        y_train=y_train,
+        y_test=y_test,
+        classifier=RandomForestClassifier(random_state=42),
+        classifier_name="rfc_model",
+        param_grid=param_grid_rfc_model,
+    )
+    logistic_model = train_model(
+        X_train=X_train,
+        X_test=X_test,
+        y_train=y_train,
+        y_test=y_test,
+        classifier=LogisticRegression(),
+        classifier_name="logistic_model",
+        param_grid=param_grid_logistic_model,
+    )
+
+    rfc_model = joblib.load("./models/rfc_model.pkl")
+    logistic_model = joblib.load("./models/logistic_model.pkl")
+
+    compare_roc_curve(models=[rfc_model, logistic_model], X_test=X_test, y_test=y_test)
+    show_features_importance(rfc_model, X_test)
+    plot_classifier_summary_as_image(
+        rfc_model, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test
+    )
--- src/plot_chart.py
+++ src/plot_chart.py
@@ -1,8 +1,13 @@
+from typing import List
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import shap
 from heatmap import corrplot
+from sklearn.base import ClassifierMixin
+from sklearn.metrics import RocCurveDisplay, classification_report
 
 
 def plot_correlations(df: pd.DataFrame) -> None:
@@ -12,7 +17,6 @@ def plot_correlations(df: pd.DataFrame) -> None:
     :param df: data
     :type df: pd.DataFrame
     """
-
     plt.figure(figsize=(10, 10))
     corrplot(df.corr())
     plt.tight_layout()
@@ -41,7 +45,6 @@ def plot_categorical_proportion(df: pd.DataFrame, column_name: str) -> None:
     :param column_name: name of the column to process
     :type column_name: str
     """
-
     # fmt: off
     categorical = (
         df
@@ -49,19 +52,18 @@ def plot_categorical_proportion(df: pd.DataFrame, column_name: str) -> None:
         .groupby([column_name, 'Attrition_Flag'])
         .count()
         .reset_index(level=1)
-        .pivot_table('CLIENTNUM', column_name, 'Attrition_Flag')
-    )
-
-    proportions = (
-        categorical
-        .apply(
-            lambda row: {'Attrited Customer': row['Attrited Customer']/sum(row),
-                        'Existing Customer': row['Existing Customer']/sum(row)},
-            axis=1)
-        .apply(pd.Series)
+        .pivot_table(values='CLIENTNUM', index=column_name, columns='Attrition_Flag')
     )
 
     # fmt: on
+
+    proportions = categorical.apply(
+        lambda row: {
+            "Attrited Customer": row["Attrited Customer"] / sum(row),
+            "Existing Customer": row["Existing Customer"] / sum(row),
+        },
+        axis=1,
+    ).apply(pd.Series)
 
     proportions.plot(kind="barh", stacked=True, colormap="tab10", figsize=(10, 6))
 
@@ -103,4 +105,122 @@ def plot_quant_histogram(df: pd.DataFrame, column_name: str) -> None:
     :type column_name: str
     """
     sns.histplot(data=df, x=column_name, bins=10)
+    plt.show()
+
+
+def compare_roc_curve(
+    list_of_models: List[ClassifierMixin], X_test: pd.DataFrame, y_test: pd.DataFrame
+) -> None:
+    """
+    Compare roc curve for given models.
+
+    :param list_of_models: list of classifiers
+    :type list_of_models: List[ClassifierMixin]
+    :param X_test: test features
+    :type X_test: pd.DataFrame
+    :param y_test: test labels
+    :type y_test: pd.DataFrame
+    """
+    first_plot = RocCurveDisplay.from_estimator(
+        estimator=list_of_models[0], X=X_test, y=y_test, alpha=0.8
+    )
+    if len(list_of_models) > 1:
+        for model in list_of_models[1:]:
+            RocCurveDisplay.from_estimator(
+                estimator=model, X=X_test, y=y_test, ax=first_plot.ax_, alpha=0.8
+            )
+
+    plt.show()
+
+
+def show_features_importance(model: ClassifierMixin, X_test: pd.DataFrame) -> None:
+    """
+    Show features importance for given tree-based model.
+
+    :param model: tree based model
+    :type model: ClassifierMixin
+    :param X_test: test features
+    :type X_test: pd.DataFrame
+    """
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X_test[:100])
+    shap.summary_plot(shap_values, X_test[:100], plot_type="bar")
+    plt.show()
+
+    # Calculate feature importances
+    importances = model.feature_importances_
+    # Sort feature importances in descending order
+    indices = np.argsort(importances)[::-1]
+
+    # Rearrange feature names so they match the sorted feature importances
+    names = [X_test.columns[i] for i in indices]
+
+    # Create plot
+    plt.figure(figsize=(20, 5))
+
+    # Create plot title
+    plt.title("Feature Importance")
+    plt.ylabel("Importance")
+
+    # Add bars
+    plt.bar(range(X_test.shape[1]), importances[indices])
+
+    # Add feature names as x-axis labels
+    plt.xticks(range(X_test.shape[1]), names, rotation=90)
+    plt.show()
+
+
+def plot_classifier_summary_as_image(
+    model: ClassifierMixin,
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.Series,
+    y_test: pd.Series,
+) -> None:
+    """
+    Make classifier report in image form.
+
+    :param model: classifier
+    :type model: ClassifierMixin
+    :param X_train: train features
+    :type X_train: pd.DataFrame
+    :param X_test: test features
+    :type X_test: pd.DataFrame
+    :param y_train: train labels
+    :type y_train: pd.Series
+    :param y_test: test labels
+    :type y_test: pd.Series
+    """
+    plt.rc("figure", figsize=(5, 5))
+    y_train_preds = model.predict(X_train)
+    y_test_preds = model.predict(X_test)
+    plt.text(
+        0.01,
+        1.25,
+        s=str("Logistic Regression Train"),
+        fontdict={"fontsize": 10},
+        fontproperties="monospace",
+    )
+    plt.text(
+        0.01,
+        0.05,
+        s=str(classification_report(y_train, y_train_preds)),
+        fontdict={"fontsize": 10},
+        fontproperties="monospace",
+    )  # approach improved by OP -> monospace!
+    plt.text(
+        0.01,
+        0.6,
+        s=str("Logistic Regression Test"),
+        fontdict={"fontsize": 10},
+        fontproperties="monospace",
+    )
+    plt.text(
+        0.01,
+        0.7,
+        s=str(classification_report(y_test, y_test_preds)),
+        fontdict={"fontsize": 10},
+        fontproperties="monospace",
+    )  # approach improved by OP -> monospace!
+    plt.axis("off")
     plt.show()
